497b8e8c935783621d78affd8753d8d503dd6ee0
Priyanshu Modi <pmw036@uregina.ca>
2026-01-31 18:34:45 -0600

    added third script to read and visualize Apple stock data with Implementing Bollinger Bands

--- read_data.py
+++ read_data.py
@@ -22,7 +22,6 @@ def read_and_visualize():
  # Plot the results and save the plot as 'apple_stock.png'.
     return df # or whatever variable you use to store the dataframe
 
-#read_and_visualize()
 
 def calculate_moving_average(df, window_size=30):
     df['Simple Moving_Average'] = df['AAPL_y'].rolling(window=window_size).mean()
@@ -34,9 +33,31 @@ def calculate_moving_average(df, window_size=30):
     plt.ylabel('Close')
     plt.title('Apple Close Pricing with 30-Day Simple Moving Average')
     plt.savefig('apple_stock_Simple_Moving_Average.png')
+    plt.close() 
+
+    return df
+
+def calculate_bollinger_bands(df, window_size=7):
+    df['Middle Band'] = df['AAPL_y'].rolling(window=window_size).mean()
+    df['Standard Deviation'] = df['AAPL_y'].rolling(window=window_size).std()
+    df['Upper Band'] = df['Middle Band'] + (df['Standard Deviation'] * 2)
+    df['Lower Band'] = df['Middle Band'] - (df['Standard Deviation'] * 2)
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(pd.to_datetime(df['AAPL_x']), df['AAPL_y'])
+    plt.plot(pd.to_datetime(df['AAPL_x']), df['Middle Band'], label='Middle Band', color='orange')
+    plt.plot(pd.to_datetime(df['AAPL_x']), df['Upper Band'], label='Upper Band', color='green')
+    plt.plot(pd.to_datetime(df['AAPL_x']), df['Lower Band'], label='Lower Band', color='red')
+    #plt.fill_between(pd.to_datetime(df['AAPL_x']), df['Lower Band'], df['Upper Band'], color='gray', alpha=0.3)
+    plt.xlabel('Date')
+    plt.ylabel('Close')
+    plt.title('Apple Close Pricing with Implementing Bollinger Bands')
+    plt.legend()
+    plt.savefig('apple_stock_Bollinger_Bands.png')
     plt.show() 
 
     return df
 
 df = read_and_visualize()
 calculate_moving_average(df, window_size=30)
+calculate_bollinger_bands(df, window_size=7)
